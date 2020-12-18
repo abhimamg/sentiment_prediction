f5f4f23aeb2fdf5b4082d04226c973182b8ff0e9
Abhishek Mamgain <abhi.mamg@gmail.com>
2020-12-18 21:17:19 +0000

    pass

--- test.py
+++ test.py
@@ -20,17 +20,12 @@ cv = pickle.load(open(path+filename, 'rb'))
 
 text_inp = [st.text_input('Enter Text', "I love the pizza")]
 
-
 tranformed_text = cv.transform(text_inp)
 
 result = lr_model.predict(tranformed_text)
 prob = lr_model.predict_proba(tranformed_text).max().round(4)
 prob = (prob*100).round(2)
-confidence_message = "Confidence = {}%".format(prob)
-
-
-# st.write(result)
-# st.write(prob)
+confidence_message = "Confidence: {}%".format(prob)
 
 if st.button('Classify Text'):
     if result == 1:
@@ -40,35 +35,3 @@ if st.button('Classify Text'):
         st.error("Sentiment: Negative")
         st.info(confidence_message)  
 
-
-
-# st.write("""
-# # Assessemnt 3: Sentiment Prediction
-# ### Submitted by: Abhishek Mamgain
-# #
-# """)
-
-# text_inp = st.text_input(
-#     'Enter Text', "I really like this food")
-
-# score = vader.polarity_scores(text_inp)
-
-# comp_score = score["compound"]
-# pos_score = round(score["pos"]*100,2)
-# neg_score = round(score["neg"]*100,2)
-# neu_score = round(score["neu"]*100,2)
-# pos_message = "Confidence = {}%".format(pos_score)
-# neg_message = "Confidence = {}%".format(neg_score)
-# neu_message = "Confidence = {}%".format(neu_score)
-
-
-# if st.button('Classify Text'):
-#     if pos_score > 0.4:
-#         st.success("Sentiment: Positive")
-#         st.info(pos_message)
-#     elif neg_score > 0.4:
-#         st.error("Sentiment = Negative")
-#         st.info(neg_message)     
-#     else:
-#         st.warning("Sentiment = Neutral")
-#         st.info(neu_message)  
